2bd6f0bf1f15016921c04c7c0612f1bfba66b420
Gerald Siegert <gerald.siegert@outlook.com>
2017-12-27 15:22:46 +0100

    autosave 27.12.2017-15:22:46

---
+++ listings/ssExample.cs
@@ -0,0 +1,14 @@
+public class YarnNode : Component
+{
+    // fault definition, also possible: new PermanentFault()
+    public readonly Fault NodeConnectionError = new TransientFault();
+    
+    // interaction logic (Members, Properties, Methods...)
+    
+    // fault effect
+    [FaultEffect(Fault = nameof(NodeConnectionError))]
+    internal class NodeConnectionErrorEffect : YarnNode
+    {
+    // fault effect logic
+    }
+}
