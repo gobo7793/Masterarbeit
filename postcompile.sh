bfacc4ccc34624819dd23f77b08ee5d653155190
Gerald Siegert <siegerge@swtse144.informatik.uni-augsburg.de>
2018-02-12 17:03:49 +0100

    autosave %date%-%time:~0,8%

---
+++ postcompile.sh
@@ -0,0 +1,7 @@
+!#bin/bash
+# commands to execute after compilation
+# settings for TeXstudio: https://tex.stackexchange.com/a/261721
+
+git add --all
+git commit -m "autosave %date%-%time:~0,8%"
+git push
