443e8c0fff86bc7196512261246ccf9c4e74a36f
Gerald Siegert <gerald.s.online@outlook.com>
2018-03-08 10:43:52 +0100

    autosave 2018-03-08 10:43:51

--- postcompile.sh
+++ postcompile.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 # commands to execute after compilation
 # settings for TeXstudio: https://tex.stackexchange.com/a/261721
-datetime=date "+%x %X"
+datetime=$(date "+%F %T")
 
 git add --all \
 	 && git commit -m "autosave $datetime" \
