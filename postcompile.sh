e1f0c9498468cc205f651982287869ff8a7b8969
Gerald Siegert <gerald.siegert@outlook.com>
2018-03-08 10:32:47 +0100

    autosave 08.03.2018-10:32:47

--- postcompile.sh
+++ postcompile.sh
@@ -1,7 +1,8 @@
 #!bin/bash
 # commands to execute after compilation
 # settings for TeXstudio: https://tex.stackexchange.com/a/261721
+datetime=date "+%x %X"
 
-git add --all
-git commit -m "autosave %date%-%time:~0,8%"
-git push
+git add --all \
+	 && git commit -m "autosave $datetime" \
+	 && git push
--- precompile.bat
+++ precompile.bat
@@ -1,4 +1,5 @@
 REM commands to execute before tex compilation
-REM copy files from ssharp repo
+git pull
 
+REM copy files from ssharp repo
 xcopy "..\ssharp\Models\TestingHadoop\YarnModel.png" "images\yarnModel.png*" /d /y
