94947a529d74e2c15417c73f930ca52155e254d9
Gerald Siegert <gerald.s.online@outlook.com>
2018-06-12 12:04:24 +0200

    updates test implementation

--- listings/executeTestCases.cs
+++ listings/executeTestCases.cs
@@ -1,37 +1,30 @@
 [Test]
 [TestCaseSource(nameof(GetTestCases))]
-public void ExecuteCaseStudy(int benchmarkSeed,
-    double faultProbability, int hostsCount, int clientCount,
-    int stepCount, bool isMutated)
+public void ExecuteCaseStudy(int benchmarkSeed, double faultProbability, int hostsCount, int clientCount, int stepCount, bool isMutated)
 {
   // Write test case parameter to log
-  
-  StartCluster(hostsCount, isMutated);
-  Thread.Sleep(7000); // wait for startup
-  
-  Logger.Info("Setting up test case");
-  var simTest = new SimulationTests
+
+  InitInstances();
+  var isFailed = false;
+  try
   {
-      MinStepTime = new TimeSpan(0, 0, 0, 25),
-      RecreatePreInputs = true,
-      PrecreatedInputs = true,
-      NodeBaseCount = 4,
-  
-      BenchmarkSeed = benchmarkSeed,
-      FaultActivationProbability = faultProbability,
-      FaultRepairProbability = faultProbability,
-      HostsCount = hostsCount,
-      ClientCount = clientCount,
-  
-      StepCount = stepCount,
-  };
-  
-  // execute
-  simTest.SimulateHadoopFaults();
-  
-  StopCluster();
-  
-  // move logs
-  MoveCaseStudyLogs(benchmarkSeed, faultProbability, hostsCount,
-      clientCount, stepCount, isMutated);
+    // Setup
+    StartCluster(hostsCount, isMutated);
+    Thread.Sleep(5000); // wait for startup
+
+    var simTest = new SimulationTests();
+    // save test case parameter to simTest
+
+    // Execution
+    simTest.SimulateHadoopFaults();
+  }
+  // catch exceptions during simulation and set isFailed=true
+  finally
+  {
+    // Teardown
+    StopCluster();
+    ResetInstances();
+    MoveCaseStudyLogs(/* test case parameter */);
+  }
+  Assert.False(isFailed);
 }
--- listings/moveTestCaseLogs.cs
+++ listings/moveTestCaseLogs.cs
@@ -1,4 +1,4 @@
 var todayStrShort = DateTime.Today.ToString("yyMMdd");
-var mutatedInt = isMutated ? 1 : 0;
+var mutated = isMutated ? 'T' : 'F';
 var faultProbStr = faultProbability.ToString(CultureInfo.InvariantCulture);
-var filename = $"{benchmarkSeed:X8}-{faultProbStr}-{hostsCount:D1}-{clientCount:D1}-{stepCount:D2}-{mutatedInt:D1}-{todayStrShort}";
+var filename = $"{benchmarkSeed:X8}-{faultProbStr}-{hostsCount:D1}-{clientCount:D1}-{stepCount:D2}-{mutated}-{todayStrShort}";
