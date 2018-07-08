5deaf56b87b49ccaa27c337694c3a2a30cbddbb7
Gerald Siegert <gerald.siegert@outlook.com>
2018-07-08 21:56:13 +0200

    merged all resources to one dir, using inline lsts and cref

--- listings/moveTestCaseLogs.cs
+++
@@ -1,4 +0,0 @@
-var todayStrShort = DateTime.Today.ToString("yyMMdd");
-var mutated = isMutated ? "MT" : "MF";
-var faultProbStr = faultProbability.ToString(CultureInfo.InvariantCulture);
-var baseFileName = $"0x{benchmarkSeed:X8}-{faultProbStr}F-{hostsCount:D1}H-{clientCount:D1}C-{stepCount:D2}S-{mutated}-{todayStrShort}";
